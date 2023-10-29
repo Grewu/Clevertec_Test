d1999cb322b236661456bc4db64bc6d6887651a0
Grewu <pavelgurevich97@gmail.com>
2023-10-29 13:54:58 +0300

    init master

---
+++ src/main/java/ru/clevertec/product/data/InfoProductDto.java
@@ -0,0 +1,25 @@
+package ru.clevertec.product.data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record InfoProductDto(
+
+
+        UUID uuid,
+
+        /**
+         * Имя продукта смотрите {@link ru.clevertec.product.entity.Product}
+         */
+        String name,
+
+        /**
+         * Описание продукта не может быть null, может быть пустой строкой
+         */
+        String description,
+
+        /**
+         * Стоимость не может быть null или негативным
+         */
+        BigDecimal price) {
+}
